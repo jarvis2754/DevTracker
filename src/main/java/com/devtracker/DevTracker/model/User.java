761026864624478ead40668b036d26a144254add
javi2754 <jjaaiiggaanneesshh77@gmail.com>
2025-07-17 12:35:00 +0530

    added primarykey for the table

--- src/main/java/com/devtracker/DevTracker/model/User.java
+++ src/main/java/com/devtracker/DevTracker/model/User.java
@@ -1,10 +1,7 @@
 package com.devtracker.DevTracker.model;
 
 import com.devtracker.DevTracker.model.enums.Position;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name="user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
     private String userName;
     private String email;
