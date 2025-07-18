1413d6ab8e02bf23ee20bec311fb54c2e9c2a6f4
jaganathan-2001 <jaganathanmani09@gmail.com>
2025-07-18 13:01:35 +0530

    added project entity with project status constants

---
+++ src/main/java/com/devtracker/DevTracker/model/Project.java
@@ -0,0 +1,52 @@
+package com.devtracker.DevTracker.model;
+
+import com.devtracker.DevTracker.model.enums.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int projectId;
+
+    private String projectName;
+    private String projectDesc;
+
+    private Date createdAt;
+
+    private Date deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "team_lead_id")
+    private User teamLead;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_members",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> teamMembers;
+
+    @Enumerated(EnumType.STRING)
+    private ProjectStatus status;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Issue> issues;
+
+
+
+    // Getters and setters
+}
